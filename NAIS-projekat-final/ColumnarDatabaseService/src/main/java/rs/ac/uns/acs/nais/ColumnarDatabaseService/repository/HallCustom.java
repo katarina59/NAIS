a290654b4e73fbf540923ba92cbde471290ac6ad
anabb55 <75089113+anabb55@users.noreply.github.com>
2024-06-26 22:25:58 +0200

    Delete files

--- NAIS-projekat-final/ColumnarDatabaseService/src/main/java/rs/ac/uns/acs/nais/ColumnarDatabaseService/repository/HallCustom.java
+++
@@ -1,7 +0,0 @@
-package rs.ac.uns.acs.nais.ColumnarDatabaseService.repository;
-
-import rs.ac.uns.acs.nais.ColumnarDatabaseService.dto.HallStatisticsDTO;
-
-public interface HallCustom {
-    HallStatisticsDTO findHallStatisticsByLocation(String location);
-}
--- NAIS-projekat-final/ColumnarDatabaseService/src/main/java/rs/ac/uns/acs/nais/ColumnarDatabaseService/repository/UserCustom.java
+++
@@ -1,9 +0,0 @@
-package rs.ac.uns.acs.nais.ColumnarDatabaseService.repository;
-
-import rs.ac.uns.acs.nais.ColumnarDatabaseService.dto.UserSessionStatisticsDTO;
-
-import java.util.List;
-
-public interface UserCustom {
-    List<UserSessionStatisticsDTO> findUserSessionStatistics();
-}
